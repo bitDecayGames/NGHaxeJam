e6d7866a0e20bab590c17e41549ebfc708f04c6e
Logan <monday.hopscotch@gmail.com>
2020-03-28 20:21:08 -0600

    ultra-rudimentary sprite render. behold the first tower

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,21 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var totem:FlxSprite; 
+	
 	override public function create():Void
 	{
 		super.create();
+		totem = new FlxSprite();
+		totem.x = FlxG.width/2;
+		totem.y = FlxG.height/2;
+		totem.loadGraphic(AssetPaths.dock_totem__png, false, 64, 64);
+		add(totem);
 	}
 
 	override public function update(elapsed:Float):Void
