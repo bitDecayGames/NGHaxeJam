b6c79e0c242d8dff2576036ac4e7dd976f99b54b
mwingfield <mwingfie@gmail.com>
2020-03-31 20:01:06 -0600

    use the flx health instead of custom life

--- source/entities/SimpleShot.hx
+++ source/entities/SimpleShot.hx
@@ -5,20 +5,19 @@ import flixel.math.FlxVector;
 import flixel.FlxSprite;
 
 class SimpleShot extends FlxSprite {
-	private var life:Float = 2;
-
 	public function new(direction:FlxVector, speed:Float, life:Float) {
 		super();
-		this.life = life;
+		health = life;
 		velocity = direction.normalize().scale(speed);
 		loadGraphic(AssetPaths.shot__png);
 	}
 
 	override public function update(delta:Float):Void {
 		super.update(delta);
-		life -= delta;
-		if (life <= 0) {
-			destroy();
-		}
+		hurt(delta);
+	}
+
+	override public function kill() {
+		destroy();
 	}
 }
